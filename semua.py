80afa7fc57cd2eea264ea6976037b79fa235cf7e
tendy bsc375 <63664956+cikawung@users.noreply.github.com>
2020-04-15 02:03:14 +0700

    Add files via upload

---
+++ semua.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+
+
+
+
+os.system('clear')
+os.system('figlet Hecker')
+print "======================================="
+print " Author : cikawung"
+print " Youtube: tendy wijayabsc"
+print " Github : https://github.com/cikawung"
+print "======================================="
+print
+print "[+] Menu Pilihan [+]"
+print "[1] Login tool"
+print "[2] Hubungi(wa)"
+pilih = raw_input('[?] pilih : ')
+if pilih == "1":
+	os.system('https://github.com/cikawung/Login')
+	print "Penginstallan Selesai"
+	print "Selanjutnya ketik $ cd Login"
+	print "Lalu ketik $ logintool"
+	print "[+] Penginstallan Selesai"
